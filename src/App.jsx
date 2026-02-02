ac0ecd9fc4970d267bd5a93d83fbb2cff9b4ad19
Rafael Andrade Simião <randradesimiao@gmail.com>
2026-02-02 12:31:26 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div className="auth-container">
+        <div className="auth-background">
+         <div className="gradient-orb orb-1"></div>
+         <div className="gradient-orb orb-2"></div>
+         <div className="gradient-orb orb-3"></div> 
+        </div>
+      </div>
+    </>
+  )
+}
+
